715f1290d84da05c0448789d5e384bd07333f501
Jasmine Gordon <jasminemgordon14@gmail.com>
2022-11-19 17:18:01 -0500

    commit tree 0faaf891d58655df487716063a7b9f9f683abc45

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution here!
+// Write your solution here
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+
+    cats.shift();
+}
+
+
+function appendCat(name) {
+
+    const newCats = [...cats, name];
+
+    return newCats
+}
+
+function prependCat(name) {
+    const newCats = [name, ...cats]
+
+    return newCats
+}
+
+function removeLastCat() {
+
+   const newestCats = cats.slice(0, cats.length - 1);
+
+    return newestCats 
+ }
+
+ function removeFirstCat() {
+
+    const newestCats = cats.slice(1)
+
+    return newestCats
+ }
